9a66578e9c7a5d17a22d7e1bb1604b01d5833895
Fris233 <baefris@gmail.com>
2025-10-31 16:45:41 +0200

    Initial Skeleton

---
+++ src/main/java/com/fhm/take2/HomePageController.java
@@ -0,0 +1,23 @@
+package com.fhm.take2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomePageController {
+
+    @FXML
+    private Button CreateSubcredditButton;
+
+    @FXML
+    private Button CredditButton;
+
+    @FXML
+    private Button HomeButton;
+
+    @FXML
+    private Button RecentButton;
+
+    @FXML
+    private Button RulesButton;
+
+}
