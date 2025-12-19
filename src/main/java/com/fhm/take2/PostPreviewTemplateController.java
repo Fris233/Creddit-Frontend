cb473f5587834f6849daa9997ee1ee4cc0ae71ea
ManCityP <mohamed.a.eltaie@gmail.com>
2025-12-19 23:29:48 +0200

    ABSOLUTELY GIGANTIC HUMONGOUS INTERPLANETARY COSMIC GALACTIC CELESTIAL!!!!!!!

--- src/main/java/com/fhm/take2/PostPreviewTemplateController.java
+++ src/main/java/com/fhm/take2/PostPreviewTemplateController.java
@@ -90,6 +90,10 @@ public class PostPreviewTemplateController {
             catch (Exception e) {
                 e.printStackTrace();
             }
+            subPFP.setImage(new Image(post.GetSubcreddit().GetLogo().GetURL(), true));
+        }
+        else {
+            subPFP.setImage(new Image(post.GetAuthor().getPfp().GetURL(), true));
         }
         UpdateJoinButton();
     }
